e903bd31b307022e6961a32b7913705ca3b91d80
Felipe Macário <felipesmacario@gmail.com>
2021-06-29 17:13:18 -0300

    Aula1

---
+++ controlePontoDeAcesso/src/main/java/com/dio/controlePontoDeAcesso/ControlePontoDeAcessoApplication.java
@@ -0,0 +1,13 @@
+package com.dio.controlePontoDeAcesso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlePontoDeAcessoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlePontoDeAcessoApplication.class, args);
+	}
+
+}
---
+++ controlePontoDeAcesso/src/test/java/com/dio/controlePontoDeAcesso/ControlePontoDeAcessoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.controlePontoDeAcesso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlePontoDeAcessoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
